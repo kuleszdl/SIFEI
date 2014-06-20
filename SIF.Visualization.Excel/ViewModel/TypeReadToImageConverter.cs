b122d6abdde4ef9fa12b667ad69ae9cd7959195f
Fabian Toth <tothfn@studi.informatik.uni-stuttgart.de>
2014-06-20 12:16:06 +0200

    The icons on the violations are now colorized when it is unread

---
+++ SIF.Visualization.Excel/ViewModel/TypeReadToImageConverter.cs
@@ -0,0 +1,46 @@
+﻿using SIF.Visualization.Excel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace SIF.Visualization.Excel.ViewModel
+{
+    class TypeReadToImageConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Rule.RuleType type = (Rule.RuleType)values[0];
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            String temp;
+            switch (type)
+            {
+                case Rule.RuleType.DYNAMIC:
+                    temp = "../icons/violations/dynamic";
+                    break;
+                case Rule.RuleType.SANITY:
+                    temp = "../icons/violations/sanity";
+                    break;
+                default:
+                    temp = "../icons/violations/static";
+                    break;
+            }
+            if (values[1] is bool && !(bool)values[1])
+            {
+                temp = temp + "_unread";
+            }
+            image.UriSource = new Uri(temp + ".png", UriKind.Relative);
+            image.EndInit();
+            return image;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
