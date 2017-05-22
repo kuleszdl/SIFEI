ddc06a2aa9871c746a2226e5aea05da71832ff2e
thechinese <th3chinese@gmx.de>
2017-05-22 19:40:33 +0200

    Added Cell Picker WF

---
+++ SIF.Visualization.Excel/CellPickerWF.cs
@@ -0,0 +1,53 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Excel;
+using Office = Microsoft.Office.Core;
+using SIF.Visualization.Excel.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SIF.Visualization.Excel
+{
+    public partial class CellPickerWF : Form
+    {
+        //Excel.WorkSheet ws;
+        Microsoft.Office.Interop.Excel.Worksheet ws;         
+        public CellPickerWF()
+        {
+            InitializeComponent();
+            ShowDialog();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            //Workbook wb = Excel.CurrentWorkbook;
+            // ws = wb.activeSheet;
+            //ws = Globals.ThisAddIn.Application.ActiveSheet;
+           // ws.SelectionChange += ws_SelectionChange;
+
+        }
+
+        void ws_SelectionChange(Microsoft.Office.Interop.Excel.Range Target)
+        {
+            this.textBox1.Text = Target.Address;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            ws.SelectionChange -= ws_SelectionChange;
+        }
+
+
+
+        public Microsoft.Office.Interop.Excel.Worksheet workbook { get; set; }
+    }
+}
